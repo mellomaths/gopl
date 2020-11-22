68ef2ee145a517ddbec38fb50c1c25cf45419b9e
Matheus Mello de Lima <mellomatheuslima@gmail.com>
2020-11-22 03:14:46 -0300

    Add open file example using uniq

---
+++ tutorial/files/uniq.go
@@ -0,0 +1,38 @@
+// Uniq is inspired by the Linux command line uniq that search for duplicated lines
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func main() {
+	counts := make(map[string]int)
+	files := os.Args[1:]
+	if len(files) == 0 {
+		fmt.Println("No file provided!")
+	}
+
+	for _, filename := range files {
+		f, err := os.Open(filename)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "uniq: %v\n", err)
+			continue
+		}
+
+		input := bufio.NewScanner(f)
+		// Note: ignores potential errors of input.Err()
+		for input.Scan() {
+			line := input.Text()
+			counts[line]++
+		}
+		f.Close()
+	}
+
+	for line, n := range counts {
+		if n > 1 {
+			fmt.Printf("%d\t%s\n", n, line)
+		}
+	}
+}
